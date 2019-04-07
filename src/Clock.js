7b436e871d80e476c06162cd755f6acb0bfc04f6
Edward Timmer <edtimmer@gmail.com>
2019-04-07 19:26:20 -0400

    styles

--- src/Clock.js
+++ src/Clock.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import moment from 'moment';
 
 class Clock extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {date: new Date()};
+    this.state = {
+      date: new Date(),
+      hour: moment().format("hh").toString(),
+      minute: moment().format("mm").toString(),
+      second: moment().format("ss").toString(),
+      ampm: moment().format("a").toString(),
+    };
   }
 
   componentDidMount() {
@@ -19,15 +26,24 @@ class Clock extends React.Component {
 
   tick = () => {
     this.setState({
-      date: new Date()
+      date: new Date(),
+      hour: moment().format("hh").toString(),
+      minute: moment().format("mm").toString(),
+      second: moment().format("ss").toString(),
+      ampm: moment().format("a").toString(),
     });
   }
 
   render() {
+    const { hour, minute, second, ampm } = this.state;
     return (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      <div className="clock">        
+        <div className="clock-element-main">{hour}</div>
+        <div className="clock-element-colon">:</div>
+        <div className="clock-element-main">{minute}</div>
+        <div className="clock-element-colon">:</div>
+        <div className="clock-element-main">{second}</div>
+        <div className="clock-element-main">{ampm}</div>
       </div>
     );
   }
